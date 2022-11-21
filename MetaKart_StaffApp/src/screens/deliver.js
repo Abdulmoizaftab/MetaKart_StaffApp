560f916ca5f1783e9149fd442658f6211d4198a6
SyedUmair282 <umairsyed282@gmail.com>
2022-11-21 17:54:43 +0500

    work commit

---
+++ MetaKart_StaffApp/src/screens/deliver.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DeliverOrders = () => {
+  return (
+    <View>
+      <Text>DeliverOrders</Text>
+    </View>
+  )
+}
+
+export default DeliverOrders
+
+const styles = StyleSheet.create({})
